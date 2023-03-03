187786e5abd91385b97620ca0f512bd60de09bf7
ianean <ianean1337@gmail.com>
2023-03-03 12:52:40 -0500

    Update get_nth.s
    fixed

--- lab3/get_nth.s
+++ lab3/get_nth.s
@@ -1,21 +1,20 @@
-get_nth:
+get_nth:;takes int in r0 and index in r1, returns number at that index in r0
 PUSH {lr}
-MOV r3, r0
-MUL r3, r3, #10; preserve the value for first run of loop2
-ADD r1, r1, #1; preserve the value for first run of loop2
+  MOV r3, r0
+	MUL r3, r3, r4; preserve the value for first run of loop2
+	ADD r1, r1, #1; preserve the value for first run of loop2
 
-nthloop:
-DIV r3, r3, #10
-SUB r1, r1, #1
-CMP r1, #0
-BGT nthloop
+get_nthloop:
+	SDIV r3, r3, r4
+	SUB r1, r1, #1
+	CMP r1, #0
+	BGT get_nthloop
 
+	MOV r0, r3
+	SDIV r3, r3, r4; divide by 10, round down
+	MUL r3, r3, r4; multiply by 10, get back same magnitude but 0 in 1's place
+	SUB r1, r0, r3; subtract rounded down number from original number
 
-SDIV r3, r3, #10; divide by 10, round down
-MUL r3, r3, #10; multiply by 10, get back same magnitude but 0 in 1's place
-SUB r1, r0, r3; subtract rounded down number from original number
-
-
-MOV r0, r2
-POP {lr}
-MOV pc, lr
+	MOV r0, r1
+  POP {lr}
+	MOV pc, lr
