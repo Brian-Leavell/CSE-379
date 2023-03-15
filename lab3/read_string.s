9d776610bac27dc0ec7ae67ec9e2e096d07ccf35
ianean <ianean1337@gmail.com>
2023-03-15 14:16:04 -0400

    enchilada tasty
    lab 4 main works babyyyyy

--- lab3/read_string.s
+++ lab3/read_string.s
@@ -1,20 +1,20 @@
 read_string:
-PUSH {lr}; reads standard input and stores at address beginning at r0
-MOV r1, r0
+  PUSH {lr}; reads standard input and stores at address beginning at r0
+  MOV r1, r0
 
 read_stringloop:
-PUSH {r1}
-BL read_character
-BL output_character
-POP {r1}
-CMP r0, #0xA
-BEQ RSend
-STRB r0, [r1]
-ADD r1, r1, #1
-B read_stringloop
+  PUSH {r1}
+  BL read_character
+  BL output_character
+  POP {r1}
+  CMP r0, #0xA
+  BEQ RSend
+  STRB r0, [r1]
+  ADD r1, r1, #1
+  B read_stringloop
 
 RSend:
-MOV r0, #0
-STRB r0, [r1]
-POP{lr}
-MOV pc, lr
+  MOV r0, #0
+  STRB r0, [r1]
+  POP{lr}
+  MOV pc, lr
